de6394030ce9c9a6be7ccb4bc148bed1236d966e
SrMohitMahajan <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>
2024-06-27 11:20:27 +0530

    initial setup

---
+++ src/components/CategorySlider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CategorySlider = () => {
+  return (
+    <>
+      <div>CategorySlider</div>
+    </>
+  );
+};
+
+export default CategorySlider;
---
+++ src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CategorySlider from "../components/CategorySlider";
+
+const Home = () => {
+  return (
+    <>
+      <CategorySlider />
+    </>
+  );
+};
+
+export default Home;
