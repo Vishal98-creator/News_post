9747d4b43b9608b50d531d1040ca69f1dea2bda5
viditviionn <vidit.viionn@gmail.com>
2024-06-27 15:54:45 +0530

    TOP_CATEGORIES Component

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,19 +1,41 @@
+import TopTags1 from "./assets/images/CategorySlider/TopTags1.png";
+import TopTags2 from "./assets/images/CategorySlider/TopTags2.png";
+import TopTags3 from "./assets/images/CategorySlider/TopTags3.png";
+import TopTags4 from "./assets/images/CategorySlider/TopTags4.png";
+import TopTags5 from "./assets/images/CategorySlider/TopTags5.png";
+
 export const COLORS = {
   white: "#fff",
   black: "#000",
 };
 
 export const TOP_CATEGORIES = [
+  { name: "Новини", image: TopTags1 },
+  { name: "Мода", image: TopTags2 },
   {
-    img: "",
-    title: "",
+    name: "Събития",
+    image: TopTags3,
+  },
+  { name: "Спорт", image: TopTags4 },
+  { name: "Музика", image: TopTags5 },
+  {
+    name: "Хайлайф",
+    image: TopTags1,
   },
   {
-    img: "",
-    title: "",
+    name: "Лайфстайл",
+    image: TopTags2,
   },
   {
-    img: "",
-    title: "",
+    name: "Лайфстайл",
+    image: TopTags3,
+  },
+  {
+    name: "Лайфстайл",
+    image: TopTags4,
+  },
+  {
+    name: "Лайфстайл",
+    image: TopTags5,
   },
 ];
--- src/screens/Home/Index.js
+++ src/screens/Home/Index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import TopView from "./TopView";
+// import TopView from "./TopView";
+import { TOP_CATEGORIES } from "../../constants";
+import CategorySlider from "../../components/CategorySlider";
 
 const Index = () => {
   return (
     <>
-      <TopView />
+      <CategorySlider TOP_CATEGORIES={TOP_CATEGORIES} />
+      {/* <TopView /> */}
     </>
   );
 };
